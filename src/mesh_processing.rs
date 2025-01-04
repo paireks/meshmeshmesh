a270389cfc4a6aae152429645614e81f8aea70e0
paireks <w.radaczynski@gmail.com>
2025-01-04 20:00:27 +0100

    Flipping meshes

--- src/mesh_processing.rs
+++ src/mesh_processing.rs
@@ -2,17 +2,71 @@ use crate::mesh::Mesh;
 
 impl Mesh {
 
-/*    /// Gets a new Mesh, but with all faces flipped
+    /// Gets a new Mesh, but with all faces flipped
     pub fn get_with_all_faces_flipped(&self) -> Mesh {
         let mut new_indices: Vec<usize> = Vec::<usize>::new();
         let start_indices = self.get_start_indices();
         let middle_indices = self.get_middle_indices();
         let end_indices = self.get_end_indices();
+        let number_of_faces = self.get_number_of_faces();
 
-        for index in self.indices {
-            new_indices.push(index);
+        for i in 0..number_of_faces {
+            new_indices.push(end_indices[i]);
+            new_indices.push(middle_indices[i]);
+            new_indices.push(start_indices[i]);
         }
 
         Mesh::new(self.coordinates.clone(), new_indices)
-    }*/
+    }
+}
+
+#[test]
+fn test_get_with_all_faces_flipped() {
+    let input = Mesh::new(
+        vec![
+            // Base
+            -2.0,1.0,0.0,
+            8.0,1.0,0.0,
+            8.0,11.0,0.0,
+            -2.0,11.0,0.0,
+
+            // Top
+            3.0,6.0,4.0
+        ],
+        vec![
+            // Base faces
+            0,1,2,
+            0,2,3,
+
+            // Side faces
+            0,1,4,
+            1,2,4,
+            2,3,4,
+            3,0,4
+        ]);
+    let expected = Mesh::new(
+        vec![
+            // Base
+            -2.0,1.0,0.0,
+            8.0,1.0,0.0,
+            8.0,11.0,0.0,
+            -2.0,11.0,0.0,
+
+            // Top
+            3.0,6.0,4.0
+        ],
+        vec![
+            // Base faces
+            2,1,0,
+            3,2,0,
+
+            // Side faces
+            4,1,0,
+            4,2,1,
+            4,3,2,
+            4,0,3
+        ]);
+    let actual = input.get_with_all_faces_flipped();
+
+    assert_eq!(expected.eq(&actual), true);
 }
