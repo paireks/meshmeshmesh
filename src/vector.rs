68a548307962c71747db2abe7ca05951d08006e2
paireks <w.radaczynski@gmail.com>
2025-01-05 19:39:10 +0100

    Add more Vector creation methods

--- src/vector.rs
+++ src/vector.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use crate::point::Point;
 
 /// Represents a three-dimensional vector with double-precision floating-point coordinates.
 ///
@@ -40,6 +41,82 @@ impl Vector {
     /// assert_eq!(result.z, 3.9);
     /// ```
     pub fn new(x: f64, y: f64, z: f64) -> Vector { Vector { x, y, z } }
+
+    /// Returns the X unit [Vector]
+    ///
+    /// X unit is a (1.0,0.0,0.0) Vector.
+    ///
+    /// # Example
+    /// ```
+    /// use meshmeshmesh::vector::Vector;
+    ///
+    /// let result = Vector::x_unit();
+    /// assert_eq!(result.x, 1.0);
+    /// assert_eq!(result.y, 0.0);
+    /// assert_eq!(result.z, 0.0);
+    /// ```
+    pub fn x_unit() -> Vector { Vector { x: 1.0, y: 0.0, z:0.0 } }
+
+    /// Returns the Y unit [Vector]
+    ///
+    /// Y unit is a (0.0,1.0,0.0) Vector.
+    ///
+    /// # Example
+    /// ```
+    /// use meshmeshmesh::vector::Vector;
+    ///
+    /// let result = Vector::y_unit();
+    /// assert_eq!(result.x, 0.0);
+    /// assert_eq!(result.y, 1.0);
+    /// assert_eq!(result.z, 0.0);
+    /// ```
+    pub fn y_unit() -> Vector { Vector { x: 0.0, y: 1.0, z:0.0 } }
+
+    /// Returns the Z unit [Vector]
+    ///
+    /// Z unit is a (0.0,0.0,1.0) Vector.
+    ///
+    /// # Example
+    /// ```
+    /// use meshmeshmesh::vector::Vector;
+    ///
+    /// let result = Vector::z_unit();
+    /// assert_eq!(result.x, 0.0);
+    /// assert_eq!(result.y, 0.0);
+    /// assert_eq!(result.z, 1.0);
+    /// ```
+    pub fn z_unit() -> Vector { Vector { x: 0.0, y: 0.0, z:1.0 } }
+
+    /// Returns 0,0,0 [Vector]
+    ///
+    /// # Example
+    /// ```
+    /// use meshmeshmesh::vector::Vector;
+    ///
+    /// let result = Vector::zero();
+    /// assert_eq!(result.x, 0.0);
+    /// assert_eq!(result.y, 0.0);
+    /// assert_eq!(result.z, 0.0);
+    /// ```
+    pub fn zero() -> Vector { Vector { x: 0.0, y: 0.0, z:0.0 } }
+
+    /// Returns a new [Vector] created from 2 [Point]s
+    ///
+    /// # Example
+    /// ```
+    /// use meshmeshmesh::point::Point;
+    /// use meshmeshmesh::vector::Vector;
+    ///
+    /// let start = Point::new(0.541, 4.051, -8.031);
+    /// let end = Point::new(-3.093, 11.391, 15.0341);
+    /// let result = Vector::from_2_points(start, end);
+    /// assert_eq!(result.x, -3.093-0.541);
+    /// assert_eq!(result.y, 11.391-4.051);
+    /// assert_eq!(result.z, 15.0341--8.031);
+    /// ```
+    pub fn from_2_points(start: Point, end: Point) -> Vector {
+        Vector::new(end.x - start.x, end.y - start.y, end.z - start.z)
+    }
 }
 
 #[cfg(test)]
@@ -113,5 +190,46 @@ mod tests {
         let actual = actual_result.ok().unwrap();
         let expected = Vector::new(1.5, -2.3, 3.9);
         assert_eq!(expected.eq(&actual), true);
+    }
+
+    #[test]
+    fn test_x_unit_vector() {
+        let result = Vector::x_unit();
+        assert_eq!(result.x, 1.0);
+        assert_eq!(result.y, 0.0);
+        assert_eq!(result.z, 0.0);
+    }
+    #[test]
+    fn test_y_unit_vector() {
+        let result = Vector::y_unit();
+        assert_eq!(result.x, 0.0);
+        assert_eq!(result.y, 1.0);
+        assert_eq!(result.z, 0.0);
+    }
+
+    #[test]
+    fn test_z_unit_vector() {
+        let result = Vector::z_unit();
+        assert_eq!(result.x, 0.0);
+        assert_eq!(result.y, 0.0);
+        assert_eq!(result.z, 1.0);
+    }
+
+    #[test]
+    fn test_zero_vector() {
+        let result = Vector::zero();
+        assert_eq!(result.x, 0.0);
+        assert_eq!(result.y, 0.0);
+        assert_eq!(result.z, 0.0);
+    }
+
+    #[test]
+    fn test_from_2_points() {
+        let start = Point::new(0.541, 4.051, -8.031);
+        let end = Point::new(-3.093, 11.391, 15.0341);
+        let result = Vector::from_2_points(start, end);
+        assert_eq!(result.x, -3.093-0.541);
+        assert_eq!(result.y, 11.391-4.051);
+        assert_eq!(result.z, 15.0341--8.031);
     }
 }
