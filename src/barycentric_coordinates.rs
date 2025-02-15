c61ea1f4e34bf4803afb5438010a685659e4b67c
paireks <w.radaczynski@gmail.com>
2025-02-15 14:13:23 +0100

    Add creating barycentric_coordinates from point and triangle

--- src/barycentric_coordinates.rs
+++ src/barycentric_coordinates.rs
@@ -1,3 +1,5 @@
+use crate::point::Point;
+use crate::triangle::Triangle;
 
 /// Represents a barycentric coordinates.
 ///
@@ -41,6 +43,42 @@ impl BarycentricCoordinates {
     /// assert_eq!(result.w, 0.2);
     /// ```
     pub fn new(u: f64, v: f64, w: f64) -> BarycentricCoordinates { BarycentricCoordinates { u, v, w } }
+
+    /// Converts a [Point] and [Triangle] into [BarycentricCoordinates].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use meshmeshmesh::barycentric_coordinates::BarycentricCoordinates;
+    /// use meshmeshmesh::point::Point;
+    /// use meshmeshmesh::triangle::Triangle;
+    ///
+    /// let input_triangle = Triangle::new(
+    /// Point::new(35.704653, 37.253023, -22.626602),
+    /// Point::new(-38.634947, 13.199458, 23.94433),
+    /// Point::new(-21.698671, -49.7235, -32.888206));
+    ///
+    /// let input_point = Point::new(1.922108, 12.641649, -10.707643);
+    ///
+    /// let actual = BarycentricCoordinates::new_from_point_and_triangle(input_point, input_triangle);
+    /// let expected = BarycentricCoordinates::new(0.5, 0.3, 0.2);
+    /// 
+    /// assert_eq!(actual.eq_with_tolerance(&expected, 0.001), true);
+    /// ```
+    ///
+    pub fn new_from_point_and_triangle(point: Point, triangle: Triangle) -> BarycentricCoordinates {
+        let triangles = triangle.get_divided_by_point(point);
+        let bcp_area = triangles.0.get_area();
+        let cap_area = triangles.1.get_area();
+        let abp_area = triangles.2.get_area();
+        let abc_area = triangle.get_area();
+
+        let u = bcp_area / abc_area;
+        let v = cap_area / abc_area;
+        let w = abp_area / abc_area;
+
+        BarycentricCoordinates::new(u, v, w)
+    }
 }
 
 #[cfg(test)]
@@ -53,6 +91,21 @@ mod tests {
         assert_eq!(result.u, 0.5);
         assert_eq!(result.v, 0.3);
         assert_eq!(result.w, 0.2);
+    }
+    
+    #[test]
+    fn test_new_from_point_and_triangle() {
+        let input_triangle = Triangle::new(
+        Point::new(35.704653, 37.253023, -22.626602),
+        Point::new(-38.634947, 13.199458, 23.94433),
+        Point::new(-21.698671, -49.7235, -32.888206));
+        
+        let input_point = Point::new(1.922108, 12.641649, -10.707643);
+        
+        let actual = BarycentricCoordinates::new_from_point_and_triangle(input_point, input_triangle);
+        let expected = BarycentricCoordinates::new(0.5, 0.3, 0.2);
+        
+        assert_eq!(actual.eq_with_tolerance(&expected, 0.001), true);
     }
 
     #[test]
--- src/barycentric_coordinates_analysis.rs
+++ src/barycentric_coordinates_analysis.rs
@@ -31,6 +31,49 @@ impl BarycentricCoordinates {
         let sum = self.u + self.v + self.w;
         (sum-1.0).abs() < tolerance
     }
+
+    /// Compares given [BarycentricCoordinates] to other one, but with a `f64` tolerance.
+    ///
+    /// If any coordinate absolute difference is > tolerance, then it should return `false`.
+    ///
+    /// # Examples
+    ///
+    /// In this example we can see the differences of coordinates are not > tolerance, so we expect `true`.
+    ///
+    /// ```
+    /// use meshmeshmesh::barycentric_coordinates::BarycentricCoordinates;
+    ///
+    /// let tolerance: f64 = 0.001;
+    /// let a = BarycentricCoordinates::new(0.5, 0.2, 0.3);
+    /// let b = BarycentricCoordinates::new(0.5 + 0.0005, 0.2 - 0.0005, 0.3 + 0.0009999);
+    ///
+    /// assert_eq!(a.eq_with_tolerance(&b, tolerance), true);
+    /// ```
+    ///
+    /// In this example we can see the Y-coordinate absolute difference is > tolerance, so we expect 'false'.
+    ///
+    /// ```
+    /// use meshmeshmesh::barycentric_coordinates::BarycentricCoordinates;
+    ///
+    /// let tolerance: f64 = 0.001;
+    /// let a = BarycentricCoordinates::new(0.5, 0.2, 0.3);
+    /// let b = BarycentricCoordinates::new(0.5 + 0.0005, 0.2 - 0.00101, 0.3 + 0.0009999);
+    /// assert_eq!(a.eq_with_tolerance(&b, tolerance), false);
+    /// ```
+    pub fn eq_with_tolerance(&self, other:&BarycentricCoordinates, tolerance: f64) -> bool {
+        if (self.u - other.u).abs() > tolerance {
+            false
+        }
+        else if (self.v - other.v).abs() > tolerance {
+            false
+        }
+        else if (self.w - other.w).abs() > tolerance {
+            false
+        }
+        else {
+            true
+        }
+    }
 }
 
 #[cfg(test)]
@@ -71,5 +114,45 @@ mod tests {
         let actual = input.are_normalized_within_tolerance(0.01);
 
         assert_eq!(actual, true);
+    }
+
+    #[test]
+    fn test_eq_within_tolerance_true(){
+        let tolerance: f64 = 0.001;
+        let a = BarycentricCoordinates::new(0.5, 0.2, 0.3);
+        let b = BarycentricCoordinates::new(0.5 + 0.0005, 0.2 - 0.0005, 0.3 + 0.0009999);
+        assert_eq!(a.eq_with_tolerance(&b, tolerance), true);
+    }
+
+    #[test]
+    fn test_eq_within_tolerance_different_x_false(){
+        let tolerance: f64 = 0.001;
+        let a = BarycentricCoordinates::new(0.5, 0.2, 0.3);
+        let b = BarycentricCoordinates::new(0.5 + 0.0011, 0.2 - 0.0005, 0.3 + 0.0009999);
+        assert_eq!(a.eq_with_tolerance(&b, tolerance), false);
+    }
+
+    #[test]
+    fn test_eq_within_tolerance_different_y_false(){
+        let tolerance: f64 = 0.001;
+        let a = BarycentricCoordinates::new(0.5, 0.2, 0.3);
+        let b = BarycentricCoordinates::new(0.5 + 0.0005, 0.2 - 0.00101, 0.3 + 0.0009999);
+        assert_eq!(a.eq_with_tolerance(&b, tolerance), false);
+    }
+
+    #[test]
+    fn test_eq_within_tolerance_different_z_false(){
+        let tolerance: f64 = 0.001;
+        let a = BarycentricCoordinates::new(0.5, 0.2, 0.3);
+        let b = BarycentricCoordinates::new(0.5 + 0.0005, 0.2 - 0.0005, 0.3 + 0.0013);
+        assert_eq!(a.eq_with_tolerance(&b, tolerance), false);
+    }
+
+    #[test]
+    fn test_eq_within_tolerance_different_xyz_false(){
+        let tolerance: f64 = 0.001;
+        let a = BarycentricCoordinates::new(0.5, 0.2, 0.3);
+        let b = BarycentricCoordinates::new(0.5 + 0.0011, 0.2 - 0.00101, 0.3 + 0.0013);
+        assert_eq!(a.eq_with_tolerance(&b, tolerance), false);
     }
 }
