dba537cec93eee4e475e0e39fbca317cef795916
paireks <w.radaczynski@gmail.com>
2025-05-06 15:49:57 +0200

    Add polygon2d.rs

--- src/point2d.rs
+++ src/point2d.rs
@@ -4,39 +4,39 @@
 /// # Example
 ///
 /// ```
-/// use meshmeshmesh::point2d::Point2d;
+/// use meshmeshmesh::point2d::Point2D;
 ///
-/// let result = Point2d::new(1.5, -2.3);
+/// let result = Point2D::new(1.5, -2.3);
 /// assert_eq!(result.x, 1.5);
 /// assert_eq!(result.y, -2.3);
 /// ```
 #[derive(Debug, Clone, Copy)]
-pub struct Point2d {
+pub struct Point2D {
     /// The x-coordinate of the point.
     pub x: f64,
     /// The y-coordinate of the point.
     pub y: f64,
 }
 
-impl PartialEq for Point2d {
+impl PartialEq for Point2D {
     fn eq(&self, other: &Self) -> bool {
         self.x == other.x && self.y == other.y
     }
 }
 
-impl Point2d {
-    /// Returns a new [Point2d]
+impl Point2D {
+    /// Returns a new [Point2D]
     ///
     /// # Example
     ///
     /// ```
-    /// use meshmeshmesh::point2d::Point2d;
+    /// use meshmeshmesh::point2d::Point2D;
     ///
-    /// let result = Point2d::new(1.5, -2.3);
+    /// let result = Point2D::new(1.5, -2.3);
     /// assert_eq!(result.x, 1.5);
     /// assert_eq!(result.y, -2.3);
     /// ```
-    pub fn new(x: f64, y: f64) -> Point2d { Point2d { x, y} }
+    pub fn new(x: f64, y: f64) -> Point2D { Point2D { x, y} }
 }
 
 #[cfg(test)]
@@ -45,39 +45,39 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let result = Point2d::new(1.5, -2.3);
+        let result = Point2D::new(1.5, -2.3);
         assert_eq!(result.x, 1.5);
         assert_eq!(result.y, -2.3);
     }
 
     #[test]
     fn test_partialeq_true() {
-        let a = Point2d::new(1.5, -2.3);
-        let b = Point2d::new(1.5, -2.3);
+        let a = Point2D::new(1.5, -2.3);
+        let b = Point2D::new(1.5, -2.3);
         assert_eq!(a.eq(&b), true);
         assert_eq!(b.eq(&a), true);
     }
 
     #[test]
     fn test_partialeq_first_different_false() {
-        let a = Point2d::new(1.5, -2.3);
-        let b = Point2d::new(1.501, -2.3);
+        let a = Point2D::new(1.5, -2.3);
+        let b = Point2D::new(1.501, -2.3);
         assert_eq!(a.eq(&b), false);
         assert_eq!(b.eq(&a), false);
     }
 
     #[test]
     fn test_partialeq_second_different_false() {
-        let a = Point2d::new(1.5, -2.3);
-        let b = Point2d::new(1.5, -2.5);
+        let a = Point2D::new(1.5, -2.3);
+        let b = Point2D::new(1.5, -2.5);
         assert_eq!(a.eq(&b), false);
         assert_eq!(b.eq(&a), false);
     }
 
     #[test]
     fn test_partialeq_all_different_false() {
-        let a = Point2d::new(1.5, -2.3);
-        let b = Point2d::new(-1.5, 0.0);
+        let a = Point2D::new(1.5, -2.3);
+        let b = Point2D::new(-1.5, 0.0);
         assert_eq!(a.eq(&b), false);
         assert_eq!(b.eq(&a), false);
     }
