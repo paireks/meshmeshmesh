5b4514e611ab2f50303c3ab549307c2719b6c04a
paireks <w.radaczynski@gmail.com>
2025-05-07 23:16:33 +0200

    Makes visibility of helper methods better

--- src/point2d_analysis.rs
+++ src/point2d_analysis.rs
@@ -2,7 +2,9 @@ use crate::point2d::{MonotoneVertexType, Point2D};
 use crate::vector2d::Vector2D;
 
 impl Point2D {
-    pub fn get_monotone_vertex_type_for_clockwise(&self, previous: &Point2D, next: &Point2D) -> MonotoneVertexType {
+    
+    /// Gets [MonotoneVertexType] for given vertex, assuming whole polygon is clockwise.
+    pub(crate) fn get_monotone_vertex_type_for_clockwise(&self, previous: &Point2D, next: &Point2D) -> MonotoneVertexType {
         let inner_angle = self.get_inner_angle_for_clockwise(previous, next);
 
         if self.are_both_neighbours_lower(previous, next) {
@@ -26,7 +28,9 @@ impl Point2D {
         MonotoneVertexType::Regular
     }
 
-    pub fn get_inner_angle_for_clockwise(&self, previous: &Point2D, next: &Point2D) -> f64 {
+    /// Gets value of the inner angle for the given vertex, assuming that the whole polygon 
+    /// is clockwise.
+    pub(crate) fn get_inner_angle_for_clockwise(&self, previous: &Point2D, next: &Point2D) -> f64 {
         let a = Vector2D::from_2_points(self, previous);
         let b = Vector2D::from_2_points(self, next);
         let angle = a.get_angle(&b);
@@ -38,7 +42,9 @@ impl Point2D {
         (2.0 * std::f64::consts::PI) - angle
     }
 
-    pub fn is_clockwise(&self, previous: &Point2D, next: &Point2D) -> bool {
+    /// Check if the segment constructed with previous, self and next vertex is clockwise,
+    /// used for calculation of inner angle.
+    fn is_clockwise(&self, previous: &Point2D, next: &Point2D) -> bool {
         let mut sum = 0.0;
 
         let x1 = previous.x;
@@ -62,11 +68,15 @@ impl Point2D {
         sum >= 0.0
     }
 
-    pub fn are_both_neighbours_lower(&self, first: &Point2D, second: &Point2D) -> bool {
+    /// Check used for Monotones vertex if the neighbouring vertices are lower than the one
+    /// checking
+    fn are_both_neighbours_lower(&self, first: &Point2D, second: &Point2D) -> bool {
         first.y < self.y && second.y < self.y
     }
 
-    pub fn are_both_neighbours_higher(&self, first: &Point2D, second: &Point2D) -> bool {
+    /// Check used for Monotones vertex if the neighbouring vertices are higher than the one
+    /// checking
+    fn are_both_neighbours_higher(&self, first: &Point2D, second: &Point2D) -> bool {
         first.y > self.y && second.y > self.y
     }
 }
--- src/polygon2d_analysis.rs
+++ src/polygon2d_analysis.rs
@@ -54,57 +54,7 @@ impl Polygon2D {
     }
 
     /// Gets [MonotoneVertexType]s for given [Polygon2D] assuming it's clockwise.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use meshmeshmesh::point2d::{MonotoneVertexType, Point2D};
-    /// use meshmeshmesh::polygon2d::Polygon2D;
-    ///
-    /// let input = Polygon2D::new(vec![
-    ///    Point2D::new(-5.981672, 50.875287),
-    ///    Point2D::new(3.075768, 55.323137),
-    ///    Point2D::new(7.725793, 50.996592),
-    ///    Point2D::new(15.044527, 59.892292),
-    ///    Point2D::new(13.184517, 53.665302),
-    ///    Point2D::new(17.025842, 49.055712),
-    ///    Point2D::new(16.864102, 41.777413),
-    ///    Point2D::new(12.456687, 46.063523),
-    ///    Point2D::new(12.375817, 37.208258),
-    ///    Point2D::new(7.829037, 32.495452),
-    ///    Point2D::new(3.106803, 37.191157),
-    ///    Point2D::new(-1.456255, 32.548511),
-    ///    Point2D::new(-8.141664, 35.174922),
-    ///    Point2D::new(-10.590682, 46.392687),
-    ///    Point2D::new(-5.091522, 42.510927),
-    ///    Point2D::new(-1.290632, 46.433122),
-    /// ]);
-    ///
-    /// let actual = input.get_monotone_vertices_types_for_clockwise();
-    ///
-    /// let expected: Vec<MonotoneVertexType> = vec![
-    ///     MonotoneVertexType::Regular,
-    ///     MonotoneVertexType::Start,
-    ///     MonotoneVertexType::Merge,
-    ///     MonotoneVertexType::Start,
-    ///     MonotoneVertexType::Regular,
-    ///     MonotoneVertexType::Regular,
-    ///     MonotoneVertexType::End,
-    ///     MonotoneVertexType::Split,
-    ///     MonotoneVertexType::Regular,
-    ///     MonotoneVertexType::End,
-    ///     MonotoneVertexType::Split,
-    ///     MonotoneVertexType::End,
-    ///     MonotoneVertexType::Regular,
-    ///     MonotoneVertexType::Start,
-    ///     MonotoneVertexType::Merge,
-    ///     MonotoneVertexType::Regular
-    /// ];
-    ///
-    /// assert_eq!(expected, actual);
-    ///
-    /// ```
-    pub fn get_monotone_vertices_types_for_clockwise(&self) -> Vec<MonotoneVertexType> {
+    pub(crate) fn get_monotone_vertices_types_for_clockwise(&self) -> Vec<MonotoneVertexType> {
         let number_of_vertices = self.vertices.len();
         let mut monotone_vertex_types: Vec<MonotoneVertexType> = Vec::with_capacity(number_of_vertices);
 
