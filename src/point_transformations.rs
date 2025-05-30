50efd42a5022fc1cb223538601821eb2610cb855
paireks <w.radaczynski@gmail.com>
2025-05-30 20:13:33 +0200

    Getting Point in local system

--- src/point_transformations.rs
+++ src/point_transformations.rs
@@ -1,4 +1,5 @@
 use std::ops;
+use crate::local_coordinate_system::LocalCoordinateSystem;
 use crate::point::Point;
 use crate::vector::Vector;
 
@@ -58,6 +59,35 @@ impl ops::Sub<Vector> for Point {
     }
 }
 
+impl Point {
+    /// Creates a new [Point], but with coordinates in the given [LocalCoordinateSystem].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use meshmeshmesh::local_coordinate_system::LocalCoordinateSystem;
+    /// use meshmeshmesh::point::Point;
+    /// use meshmeshmesh::vector::Vector;
+    ///
+    /// let input = Point::new(44.545922, 27.392431, 12.289269);
+    /// let local_coordinate_system = LocalCoordinateSystem::new(
+    ///     Point::new(-43.836955, -22.211852, 10.0),
+    ///     Vector::new(0.721276,0.692648,0.0),
+    ///     Vector::new(-0.290878,0.3029,0.907547)
+    /// );
+    ///
+    /// let expected = Point::new(-11.949745, 8.895507, 40.020804);
+    /// 
+    /// let actual = input.get_in_local_coordinate_system(&local_coordinate_system);
+    /// 
+    /// assert!(expected.eq_with_tolerance(&actual, 0.001));
+    /// 
+    /// ```
+    pub fn get_in_local_coordinate_system(&self, local_coordinate_system: &LocalCoordinateSystem) -> Point {
+        local_coordinate_system.origin + local_coordinate_system.x * self.x + local_coordinate_system.y * self.y + local_coordinate_system.z * self.z
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -78,5 +108,21 @@ mod tests {
         let result = a - b;
         let expected = Point::new(5.231-(-12.564), -0.341-5.642, 11.034-7.731);
         assert_eq!(result.eq(&expected), true);
+    }
+    
+    #[test]
+    fn test_get_in_local_coordinate_system() {
+        let input = Point::new(44.545922, 27.392431, 12.289269);
+        let local_coordinate_system = LocalCoordinateSystem::new(
+            Point::new(-43.836955, -22.211852, 10.0),
+            Vector::new(0.721276,0.692648,0.0),
+            Vector::new(-0.290878,0.3029,0.907547)
+        );
+        
+        let expected = Point::new(-11.949745, 8.895507, 40.020804);
+        
+        let actual = input.get_in_local_coordinate_system(&local_coordinate_system);
+        
+        assert!(expected.eq_with_tolerance(&actual, 0.001));
     }
 }
