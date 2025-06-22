9c6acb814f3722ddad9fceca1ff42a081d53e01c
paireks <w.radaczynski@gmail.com>
2025-06-22 19:00:30 +0200

    Remove nonexisting example for get_mesh_ids_with_face_colors_applied

--- src/scene_analysis.rs
+++ src/scene_analysis.rs
@@ -5,11 +5,6 @@ impl Scene {
     /// Gets all `id`s of the [Mesh]es for which any [Element] applied `face_colors`.
     /// 
     /// This method can be useful for additional checks.
-    /// 
-    /// # Example
-    /// 
-    /// In this example we load the [Scene] which has 2 Meshes with `face_colors`
-    /// applied (id: 0 and id: 2). That's why we expect 0 and 2 in the output `HashSet`.
     pub (crate) fn get_mesh_ids_with_face_colors_applied(&self) -> HashSet<usize> {
         let mut set: HashSet<usize> = HashSet::new();
 
