83eac3468faec1802529dee54b5014836b079a89
paireks <w.radaczynski@gmail.com>
2025-07-03 17:46:47 +0200

    get_rotated_by_quaternion_around_its_origin

--- src/local_coordinate_system_transformations.rs
+++ src/local_coordinate_system_transformations.rs
@@ -1,6 +1,5 @@
 use std::ops;
 use crate::local_coordinate_system::LocalCoordinateSystem;
-use crate::point::Point;
 use crate::quaternion::Quaternion;
 use crate::vector::Vector;
 
@@ -68,12 +67,36 @@ impl ops::Sub<Vector> for LocalCoordinateSystem {
 
 impl LocalCoordinateSystem {
 
-/*    /// Returns the rotated [LocalCoordinateSystem] using given [Quaternion].
+    /// Returns the rotated [LocalCoordinateSystem] using given [Quaternion].
     /// 
-    /// 
-    pub fn get_rotated_by_quaternion(&self, quaternion: Quaternion) -> LocalCoordinateSystem {
-        
-    }*/
+    /// It rotates this system around its own origin.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use meshmeshmesh::local_coordinate_system::LocalCoordinateSystem;
+    /// use meshmeshmesh::point::Point;
+    /// use meshmeshmesh::quaternion::Quaternion;
+    /// use meshmeshmesh::vector::Vector;
+    ///
+    /// let origin = Point::new(3.2, 5.0, -1.2);
+    /// let x = Vector::new(1.0, 0.0, 0.0);
+    /// let y = Vector::new(0.0, 1.0, 0.0);
+    /// let input = LocalCoordinateSystem::new(origin, x, y);
+    /// let quaternion = Quaternion::new(0.54418103763310099, -0.16946900809404691, -0.24431282685273129, 0.78451162911164585);
+    ///
+    /// let actual = input.get_rotated_by_quaternion_around_its_origin(quaternion);
+    /// let expected = LocalCoordinateSystem::new(origin, Vector::new(0.823183,-0.567776,6.325037e-9), Vector::new(0.198889,0.288356,0.93664));
+    ///
+    /// assert!(expected.eq_with_tolerance(&actual, 0.001));
+    ///
+    /// ```
+    pub fn get_rotated_by_quaternion_around_its_origin(&self, quaternion: Quaternion) -> LocalCoordinateSystem {
+        let rotated_x = self.x.get_rotated_by_quaternion(quaternion);
+        let rotated_y = self.y.get_rotated_by_quaternion(quaternion);
+
+        LocalCoordinateSystem::new(self.origin, rotated_x, rotated_y)
+    }
     
     /// Creates the new [LocalCoordinateSystem] but flipped, meaning the z-axis and x-axis are
     /// reversed, but the y_axis stays the same.
@@ -151,5 +174,19 @@ mod tests {
         assert_eq!(actual.origin, Point::new(0.0, 5.0, -1.2));
         assert_eq!(actual.x, Vector::new(0.0, 0.0, -1.0));
         assert_eq!(actual.y, Vector::new(0.0, -1.0, 0.0));
+    }
+    
+    #[test]
+    fn test_get_rotated_by_quaternion_around_its_origin() {
+        let origin = Point::new(3.2, 5.0, -1.2);
+        let x = Vector::new(1.0, 0.0, 0.0);
+        let y = Vector::new(0.0, 1.0, 0.0);
+        let input = LocalCoordinateSystem::new(origin, x, y);
+        let quaternion = Quaternion::new(0.54418103763310099, -0.16946900809404691, -0.24431282685273129, 0.78451162911164585);
+        
+        let actual = input.get_rotated_by_quaternion_around_its_origin(quaternion);
+        let expected = LocalCoordinateSystem::new(origin, Vector::new(0.823183,-0.567776,6.325037e-9), Vector::new(0.198889,0.288356,0.93664));
+        
+        assert!(expected.eq_with_tolerance(&actual, 0.001));
     }
 }
